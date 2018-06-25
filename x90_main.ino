3d3424af2bf3d33e607ce5fd854a10e6a78b58dd
Jindrich Dolezy <jindrich.dolezy@strv.com>
2018-06-25 13:11:59 +0200

    Detect and fail if button is broken when starting up

--- x90_main.ino
+++ x90_main.ino
@@ -8,6 +8,21 @@ void setup() {
 
   highscoreLoad();
 
+  uint8_t scan = ioScan(5);
+  if (scan != 0) {
+    dispSetImage(imgError);
+
+    uint8_t b = 0;
+    while ((scan & 1) == 0) {
+      b++;
+      scan >>= 1;
+    }
+    
+    dispShowDigit(b, 3);
+    dispUpdate();
+    while (1);
+  }
+
   modeSet(&modeReady);
 }
 
