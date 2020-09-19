a70c7c2c6a200f520d091de0c7e4a87418e6980c
Jindrich Dolezy <jindrich.dolezy@strv.com>
2020-09-19 16:16:33 +0200

    Add highscore reset, better button release code

--- bumbum.ino
+++ bumbum.ino
@@ -6,15 +6,15 @@
 // cut wav headers in hexfiend
 // xxd -i out.wav > out.h
 
-#define PIN_LED_1 4
-#define PIN_LED_2 5
-#define PIN_LED_3 6
-#define PIN_LED_4 7
+#define PIN_LED_1 6
+#define PIN_LED_2 7
+#define PIN_LED_3 4
+#define PIN_LED_4 5
 
-#define PIN_BTN_1 8
-#define PIN_BTN_2 9
-#define PIN_BTN_3 10
-#define PIN_BTN_4 12
+#define PIN_BTN_1 12
+#define PIN_BTN_2 10
+#define PIN_BTN_3 9
+#define PIN_BTN_4 8
 
 #define PIN_SPEAKER 11
 
--- x80_modes.ino
+++ x80_modes.ino
@@ -6,7 +6,9 @@ void (*modeCurrent)();
 // initial mode, just shows image and highscore and goes to countdown
 void modeReady() {
   dispSetImage(imgStart);
-  delay(1000); // wait a bit for button release after fail mode
+  while (ioScan(1) != 0) {} // wait a bit for button release after fail mode
+  delay(500);
+
 
   while (true) {
     dispShowNumber(highscoreAmount);
@@ -19,22 +21,36 @@ void modeReady() {
 // 3 second countdown with sound and images, then goes to buttonWait mode
 void modeCountdown() {
   mScore = 0;
-  ioLedMask(15);
 
+  uint8_t resetStage = 0;
+
+  ioLedMask(15);
   dispSetImage(imgCountdown3);
   playbackStart(sndBtn, sizeof(sndBtn));
   delay(1000);
 
+  if (ioScan(1) == 5) resetStage++;
+
+  ioLedMask(6);
   dispSetImage(imgCountdown2);
   playbackStart(sndBtn, sizeof(sndBtn));
   delay(1000);
 
+  if (ioScan(1) == 10) resetStage++;
+
+  ioLedMask(0);
   dispSetImage(imgCountdown1);
   playbackStart(sndBtn, sizeof(sndBtn));
   delay(1000);
 
-  playbackStart(sndBtn, sizeof(sndBtn));
-  modeSet(&modeButtonWait);
+  if (ioScan(1) == 5) resetStage++;
+
+  if (resetStage < 3) {
+    playbackStart(sndBtn, sizeof(sndBtn));
+    modeSet(&modeButtonWait);
+  } else {
+    modeSet(&modeReset);
+  }
 }
 
 // picks one button at random, lights up the correct led, shows hint image and waits for pressing this button
@@ -62,7 +78,7 @@ void modeButtonWait() {
 // plays sound, shows highscore and then goes to buttonWait again
 void modeButtonOk() {
   playbackStart(sndBtn, sizeof(sndBtn));
-  ioLedMask(15);
+  ioLedMask(0);
 
   mScore++;
   dispShowNumber(mScore);
@@ -71,27 +87,40 @@ void modeButtonOk() {
   modeSet(&modeButtonWait);
 }
 
-// plays sound, checks highscore and mocks the player. goes to ready 
+// plays sound, checks highscore and mocks the player. goes to ready
 void modeFail() {
   playbackStart(sndFail, sizeof(sndFail));
-
-  if (mScore > highscoreAmount) {
+  bool isHi = mScore > highscoreAmount;
+  if (isHi) {
     dispSetImage(imgHiscore);
     highscoreSet(mScore);
   } else {
     dispSetImage(imgFail);
   }
 
-  delay(3000); // wait some time for button release
+  while (ioScan(1) != 0) {}
+  delay(1500); // wait some time for button release
 
   uint8_t cnt = 10;
   while (cnt--) {
     dispShowNumber(mScore);
     if (modeBlinkAndWait(3, 20, 10, &modeReady)) return;
-    dispSetImage(imgFail);
+    if (isHi) {
+      dispSetImage(imgHiscore);
+    } else {
+      dispSetImage(imgFail);
+    }
     if (modeBlinkAndWait(3, 20, 10, &modeReady)) return;
   }
 
+  modeSet(&modeReady);
+}
+
+void modeReset() {
+  highscoreSet(0);
+  dispSetImage(imgReset);
+  while (ioScan(1) != 0) {} // wait a bit for button release after fail mode
+  delay(2000);
   modeSet(&modeReady);
 }
 
